c0cc64a99f8f5a3aa65089e04c13e702e53b994a
Kia Boesen Thomsen <201805069@post.au.dk>
2020-02-19 14:22:39 +0100

    Stub og mock tilføjet (manuelt)

--- ECSUnitTest/UnitTest1.cs
+++ ECSUnitTest/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace ECSUnitTest
         {
             _thr = 1;
             _fakeHeater = new FakeHeater();
-            _fakeSensor = new FakeSensor1();
+            _fakeSensor = new FakeSensor25deg();
             UUT = new ECS(_thr,_fakeSensor,_fakeHeater);
         }
 
@@ -30,34 +30,54 @@ namespace ECSUnitTest
         }
     }
 
-    public class FakeSensor1 : ISensor
+    public class FakeSensor25deg : ISensor
     {
+        public int testRun { get; private set; }
+
+        public FakeSensor25deg()
+        {
+            testRun = 0;
+        }
+
         public int GetTemp()
         {
-            throw new NotImplementedException();
+            return 25;
         }
 
         public bool RunSelfTest()
         {
-            throw new NotImplementedException();
+            testRun++;
+            return true;
         }
     }
 
     public class FakeHeater : IHeater
     {
+        public int TurnedOn { get; private set; }
+        public int TurnedOff { get; private set; }
+        public int TestRun { get; private set; }
+
+        public FakeHeater()
+        {
+            TurnedOn = 0;
+            TurnedOff = 0;
+            TestRun = 0;
+        }
+
         public void TurnOn()
         {
-            throw new NotImplementedException();
+            TurnedOn++;
         }
 
         public void TurnOff()
         {
-            throw new NotImplementedException();
+            TurnedOff++;
         }
 
         public bool RunSelfTest()
         {
-            throw new NotImplementedException();
+            TestRun++;
+            return true;
         }
     }
 }
