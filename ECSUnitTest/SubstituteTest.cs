426711f7cf6ec46556f4e28d7b24384a2ed81329
Mie Tollund Sørensen <au586191@uni.au.dk>
2020-02-19 15:34:26 +0100

    Oprettelse af interaction-based tets

--- ECSUnitTest/SubstituteTest.cs
+++ ECSUnitTest/SubstituteTest.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECSNew;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ECSUnitTest
 {
-    class SubstituteTest
+    public class SubstituteTest
     {
+        public class UnitTest1
+        {
+            private int _thr;
+            private ISensor _fakeSensor;
+            private IHeater _fakeHeater;
+            private ECS UUT;
+
+
+            [SetUp]
+            public void Setup()
+            {
+                _thr = 25;
+                _fakeSensor = Substitute.For<ISensor>(); //Create substitute, indeholder inddirekte "new" 
+                _fakeHeater = Substitute.For<IHeater>();
+
+                UUT = new ECS(_thr, _fakeSensor, _fakeHeater);
+            }
+
+
+            [Test]
+            public void TestWithSub()//Da vi asserter på en mock er det en interaction-based test
+            {
+                //setup
+                _fakeSensor.GetTemp().Returns(20);//fakeSensor fungerer som stub i denne test
+
+                //Act
+                UUT.Regulate();
+
+                //Assert
+                _fakeHeater.Received(1).TurnOn();//fakeHeater fungerer som en mock i denne test
+
+
+            }
+        }
     }
 }
